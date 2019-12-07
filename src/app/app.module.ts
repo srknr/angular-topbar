62f0573b04a4132fd5a3aac20240ed822db78298
srknr <k_srkn13@yahoo.co.jp>
2019-12-07 18:51:38 +0900

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
@@ -11,6 +13,8 @@ import { TopBarComponent } from './top-bar/top-bar.component';
   imports:      [ 
     BrowserModule, 
     FormsModule, 
+    MatIconModule,
+    FontAwesomeModule,
     RouterModule.forRoot([
       {path: "", component: ListComponent}
     ])
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-top-bar',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopBarComponent implements OnInit {
 
+  readonly icons = {
+    faCartPlus
+  };
   constructor() { }
 
   ngOnInit() {
